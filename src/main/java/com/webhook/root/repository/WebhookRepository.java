99f86cdf659a473dc0e562da5b0e5e8bd22faf20
JOSIE_BROHM <josiembrohm@gmail.com>
2025-08-11 16:31:07 +0200

    POST requests received are saved to DB instead of list. Also added a SQL script to init the DB

---
+++ src/main/java/com/webhook/root/repository/WebhookRepository.java
@@ -0,0 +1,10 @@
+package com.webhook.root.repository;
+
+import com.webhook.root.model.Webhook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WebhookRepository extends JpaRepository<Webhook, Long> {
+
+}
