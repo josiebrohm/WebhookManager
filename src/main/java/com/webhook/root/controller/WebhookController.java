01d4caf12fbd51c6baea51a441f8b28f3b20e0ad
Dylan Viranyi <dylan.viranyi@gmail.com>
2025-08-23 21:09:55 +0200

    configured docker kafka

--- src/main/java/com/webhook/root/controller/WebhookController.java
+++ src/main/java/com/webhook/root/controller/WebhookController.java
@@ -1,6 +1,7 @@
 package com.webhook.root.controller;
 
 import com.webhook.root.model.Webhook;
+import com.webhook.root.service.SimpleProducer;
 import com.webhook.root.service.WebhookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,9 @@ public class WebhookController {
         this.webhookService = webhookService;
     }
 
+    @Autowired
+    private SimpleProducer producer;
+    
     @GetMapping
     public String welcome() {
         return "Welcome! How'd you GET here?";
@@ -29,5 +33,11 @@ public class WebhookController {
     @PostMapping("/webhooks")
     public Webhook addWebhook(@RequestBody Webhook webhook) {
         return this.webhookService.saveWebhook(webhook);
+    }
+
+    @GetMapping("/send")
+    public String sendMesssage(@RequestParam String msg) {
+        producer.send(msg);
+        return "Message sent!";
     }
 }
