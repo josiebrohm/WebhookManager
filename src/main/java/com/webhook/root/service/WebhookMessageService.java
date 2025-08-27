8d0e7b909f3fe8c73f8a3cea90891f4b16b6054e
JOSIE_BROHM <josiembrohm@gmail.com>
2025-08-27 21:00:06 +0200

    Add webhook message service - maybe should be combined with all the kafka services? unsure

---
+++ src/main/java/com/webhook/root/service/WebhookMessageService.java
@@ -0,0 +1,36 @@
+package com.webhook.root.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.webhook.root.model.WebhookMessage;
+import com.webhook.root.repository.WebhookMessageRepository;
+
+@Service
+public class WebhookMessageService {
+	private final WebhookMessageRepository webhookMessageRepository;
+
+	public WebhookMessageService(WebhookMessageRepository webhookMessageRepository) {
+		this.webhookMessageRepository = webhookMessageRepository;
+	}
+
+	public WebhookMessage addWebhookMessage(WebhookMessage webhookMessage) {
+		webhookMessageRepository.save(webhookMessage);
+		return webhookMessage;
+	}
+
+	public List<WebhookMessage> getAllEndpoints() {
+		return webhookMessageRepository.findAll();
+	}
+
+	public Optional<WebhookMessage> findById(UUID id) {
+		return webhookMessageRepository.findById(id);
+	}
+
+	public void deleteById(UUID id) {
+		webhookMessageRepository.deleteById(id);
+	}
+}
